47d3a318620c28d0ef577c466cd945f6f3fc9efc
femtomc <mccoybecker@gmail.com>
2020-12-09 14:15:04 -0500

    Setting up easy script builders.

--- create_dev.sh
+++ create_dev.sh
@@ -9,21 +9,23 @@ mkdir $DEV_DIR
 cd $DEV_DIR
 
 # Build a development version of Julia with LLVM 12 and MLIR.
-#git clone https://github.com/JuliaLang/julia
-#cd julia
-#git checkout $JULIA_COMMIT_HEAD
-#make -j `nproc` \
-#    USE_BINARYBUILDER_LLVM=0 \
-#    LLVM_VER=svn \
-#    LLVM_DEBUG=2 \
-#    USE_MLIR=1
-#cd ..
+git clone https://github.com/JuliaLang/julia
+cd julia
+git checkout $JULIA_COMMIT_HEAD
+make -j `nproc` \
+    USE_BINARYBUILDER_LLVM=0 \
+    LLVM_VER=svn \
+    LLVM_DEBUG=2 \
+    USE_MLIR=1
+
+cd ..
 
 # Build MLIR.jl.
 git clone https://github.com/vchuravy/MLIR.jl
 cd MLIR.jl
-$(JULIA_PATH) create_bindings.jl
+$JULIA_PATH create_bindings.jl
 
 touch jl.sh
-$(JULIA_PATH) > jl.sh
+echo $JULIA_PATH > jl.sh
 chmod +x jl.sh
+cd ..
