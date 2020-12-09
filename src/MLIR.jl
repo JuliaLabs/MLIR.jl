6da0e5f25500e518a7cd0d5262954d23d24ce11d
femtomc <mccoybecker@gmail.com>
2020-12-08 22:21:36 -0500

    Removed LibTemplate.jl

--- src/MLIR.jl
+++ src/MLIR.jl
@@ -11,6 +11,7 @@ using ..MLIR: libmlir
 const intptr_t = Ptr{Csize_t}
 libdir = joinpath(@__DIR__, "..", "lib")
 include(joinpath(libdir, "ctypes.jl"))
+export Ctm, Ctime_t, Cclock_t
 include(joinpath(libdir, "libmlir_common.jl"))
 include(joinpath(libdir, "libmlir_api.jl"))
 end
