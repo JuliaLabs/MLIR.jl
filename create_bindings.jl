64c35209bb4b5c3b6f319451d8daf0138da3e94a
femtomc <mccoybecker@gmail.com>
2020-12-08 21:47:38 -0500

    Cleaned up erroneous arguments to clang_includes.

--- create_bindings.jl
+++ create_bindings.jl
@@ -6,6 +6,7 @@ using Clang.LibClang.Clang_jll
 const LIBCLANG_INCLUDE = joinpath(dirname(Clang_jll.libclang_path), "..", "include", "clang-c") |> normpath
 const MLIR_C_INCLUDE = joinpath(@__DIR__, "..", "julia/usr/include/mlir-c") |> normpath
 const MLIR_C_HEADERS = [joinpath(MLIR_C_INCLUDE, header) for header in readdir(MLIR_C_INCLUDE) if endswith(header, ".h")]
+
 println("Found headers:")
 display(MLIR_C_HEADERS)
 try
@@ -17,7 +18,7 @@ end
 wc = init(; headers = MLIR_C_HEADERS,
           output_file = joinpath(@__DIR__, "lib/libmlir_api.jl"),
           common_file = joinpath(@__DIR__, "lib/libmlir_common.jl"),
-          clang_includes = vcat(LIBCLANG_INCLUDE, CLANG_INCLUDE, MLIR_C_INCLUDE),
+          clang_includes = vcat(LIBCLANG_INCLUDE, CLANG_INCLUDE),
           clang_args = ["-I", joinpath(LIBCLANG_INCLUDE, ".."),
                         "-I", joinpath(MLIR_C_INCLUDE, "..")],
           header_wrapped = (root, current) -> root == current,
