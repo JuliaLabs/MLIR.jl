45fa4f42c3c69cdfad093496ec873f020c32ae23
femtomc <mccoybecker@gmail.com>
2020-12-21 01:01:53 -0500

    Corrected usage of StringRef - replace with Identifier owned by MLIR.

--- src/core/utils.jl
+++ src/core/utils.jl
@@ -1,7 +1,8 @@
+# Should not be exposed to the user.
 const StringRef = MLIR.API.MlirStringRef
-
 create_string_ref(str::String) = MLIR.API.mlirStringRefCreateFromCString(str)
 StringRef(str::String) = create_string_ref(str)
 
+# Unwraps API structs to ptr field.
 unwrap(o) = getfield(o, :ptr)
 unwrap(ptr::P) where P <: Ptr = ptr
