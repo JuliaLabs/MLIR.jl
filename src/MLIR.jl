75776fa84e1cb6cf5d9efd339334b5152d11a249
Valentin Churavy <vchuravy@users.noreply.github.com>
2023-05-16 16:42:24 -0400

    Handle MlirStringRef and Strings (#12)

--- src/MLIR.jl
+++ src/MLIR.jl
@@ -20,4 +20,19 @@ module API
     end
 end # module API
 
+# MlirStringRef is a non-owning reference to a string,
+# we thus need to ensure that the Julia string remains alive
+# over the use. For that we use the cconvert/unsafe_convert mechanism
+# for foreign-calls. The returned value of the cconvert is rooted across
+# foreign-call.
+Base.cconvert(::Type{API.MlirStringRef}, s::Union{Symbol, String}) = s
+Base.cconvert(::Type{API.MlirStringRef}, s::AbstractString) =
+    Base.cconvert(API.MlirStringRef, String(s)::String)
+
+# Directly create `MlirStringRef` instead of adding an extra ccall.
+function Base.unsafe_convert(::Type{API.MlirStringRef}, s::Union{Symbol, String, AbstractVector{UInt8}})
+    p = Base.unsafe_convert(Ptr{Cchar}, s)
+    return API.MlirStringRef(p, length(s))
+end
+
 end # module
--- test/executionengine.jl
+++ test/executionengine.jl
@@ -20,9 +20,9 @@ end
 function lowerModuleToLLVM(ctx, mod)
     pm = MLIR.API.mlirPassManagerCreate(ctx)
     if LLVM.version() >= v"15"
-        op = MLIR.API.mlirStringRefCreateFromCString("func.func")
+        op = "func.func"
     else
-        op = MLIR.API.mlirStringRefCreateFromCString("builtin.func")
+        op = "builtin.func"
     end
     opm = MLIR.API.mlirPassManagerGetNestedUnder(pm, op)
     if LLVM.version() >= v"15"
@@ -49,8 +49,7 @@ ctx = MLIR.API.mlirContextCreate()
 registerAllUpstreamDialects!(ctx)
 
 if LLVM.version() >= v"15"
-    ir = MLIR.API.mlirStringRefCreateFromCString(
-        """
+    ir = """
         module {
             func.func @add(%arg0 : i32) -> i32 attributes { llvm.emit_c_interface } {
                 %res = arith.addi %arg0, %arg0 : i32
@@ -58,10 +57,8 @@ if LLVM.version() >= v"15"
             }
         }
         """
-    )
 else
-    ir = MLIR.API.mlirStringRefCreateFromCString(
-        """
+    ir = """
         module {
             func @add(%arg0 : i32) -> i32 attributes { llvm.emit_c_interface } {
                 %res = arith.addi %arg0, %arg0 : i32
@@ -69,7 +66,6 @@ else
             }
         }
         """
-    )
 end
 mod = MLIR.API.mlirModuleCreateParse(ctx, ir)
 lowerModuleToLLVM(ctx, mod)
@@ -85,8 +81,7 @@ if jit == C_NULL
     error("Execution engine creation failed")
 end
 
-addr = MLIR.API.mlirExecutionEngineLookup(jit,
-    MLIR.API.mlirStringRefCreateFromCString("add"))
+addr = MLIR.API.mlirExecutionEngineLookup(jit, "add")
 
 if addr == C_NULL
     error("Lookup failed")
