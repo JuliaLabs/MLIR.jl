bc57d849784cd4560bd70f44a623c2dc8f13477c
Valentin Churavy <v.churavy@gmail.com>
2024-01-20 16:32:15 -0500

    Test show on dialect

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,10 @@ include("executionengine.jl")
 
     @test s == reconstructed
 end
+
+@testset "show" begin
+    MLIR.IR.context!(MLIR.IR.Context()) do
+        dialect = MLIR.IR.get_or_load_dialect!("func")
+        @teest sprint(show, dialect) == "Dialect(\"func\")"
+    end
+end
