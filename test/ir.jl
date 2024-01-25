fd0fb71a2467e6e83de5f24b1327e0e548922c37
jumerckx <julesmerckx12@gmail.com>
2024-01-25 22:28:19 +0100

    add tests

--- test/ir.jl
+++ test/ir.jl
@@ -1,4 +1,4 @@
-using MLIR.Dialects: arith
+using MLIR.Dialects: arith, builtin
 using MLIR.IR, LLVM
 
 @testset "operation introspection" begin
@@ -9,5 +9,18 @@ using MLIR.IR, LLVM
         @test IR.name(op) == "arith.constant"
         @test IR.dialect(op) === :arith
         @test IR.get_attribute_by_name(op, "value") |> IR.bool_value
+    end
+end
+
+@testset "Module construction from operation" begin
+    IR.context!(IR.Context()) do
+        op = builtin.module_(bodyRegion=IR.Region())
+        mod = IR.Module(op)
+        op = IR.get_operation(mod)
+
+        @test IR.name(op) == "builtin.module"
+
+        # Only a `module` operation can be used to create a module.
+        @test_throws AssertionError IR.Module(arith.constant(; value=true, result=MLIRType(Bool)))
     end
 end
