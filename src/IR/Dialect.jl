a328f898d86edac613ffd59c84e0d18bf12e2aae
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-27 15:37:22 +0100

    Refactor `Dialect` code

--- src/IR/Dialect.jl
+++ src/IR/Dialect.jl
@@ -17,16 +17,16 @@ function Base.show(io::IO, dialect::Dialect)
     print(io, "Dialect(\"", namespace(dialect), "\")")
 end
 
-allow_unregistered_dialects(ctx::Context=context()) = API.mlirContextGetAllowUnregisteredDialects(ctx)
-allow_unregistered_dialects!(ctx::Context=context(), allow::Bool=true) = API.mlirContextSetAllowUnregisteredDialects(ctx, allow)
+allow_unregistered_dialects(context::Context=context()) = API.mlirContextGetAllowUnregisteredDialects(context)
+allow_unregistered_dialects!(allow::Bool=true; context::Context=context()) = API.mlirContextSetAllowUnregisteredDialects(context, allow)
 
 num_registered_dialects(ctx::Context=context()) = API.mlirContextGetNumRegisteredDialects(ctx)
 num_loaded_dialects(ctx::Context=context()) = API.mlirContextGetNumLoadedDialects(ctx)
 
 load_all_available_dialects(ctx::Context=context()) = API.mlirContextLoadAllAvailableDialects(ctx)
 
-function get_or_load_dialect!(ctx::Context, name::String)
-    dialect = API.mlirContextGetOrLoadDialect(ctx, name)
+function get_or_load_dialect!(name::String; context::Context=context())
+    dialect = API.mlirContextGetOrLoadDialect(context, name)
     API.mlirDialectIsNull(dialect) && error("could not load dialect $name")
     Dialect(dialect)
 end
@@ -44,18 +44,16 @@ Base.convert(::Core.Type{API.MlirDialectHandle}, handle::DialectHandle) = handle
 
 namespace(handle::DialectHandle) = String(API.mlirDialectHandleGetNamespace(handle))
 
-function get_or_load_dialect!(ctx::Context, handle::DialectHandle)
-    dialect = API.mlirDialectHandleLoadDialect(handle, ctx)
+function get_or_load_dialect!(handle::DialectHandle; context::Context=context())
+    dialect = API.mlirDialectHandleLoadDialect(handle, context)
     API.mlirDialectIsNull(dialect) && error("could not load dialect from handle $handle")
     Dialect(dialect)
 end
 
-function get_or_load_dialect!(dialect::String)
-    get_or_load_dialect!(DialectHandle(Symbol(dialect)))
-end
+get_or_load_dialect!(dialect::String; context::Context=context()) = get_or_load_dialect!(DialectHandle(Symbol(dialect)); context)
 
-register_dialect!(ctx::Context, handle::DialectHandle) = API.mlirDialectHandleRegisterDialect(handle, ctx)
-load_dialect!(ctx::Context, handle::DialectHandle) = Dialect(API.mlirDialectHandleLoadDialect(handle, ctx))
+register_dialect!(handle::DialectHandle; context::Context=context()) = API.mlirDialectHandleRegisterDialect(handle, context)
+load_dialect!(handle::DialectHandle; context::Context=context()) = Dialect(API.mlirDialectHandleLoadDialect(handle, context))
 
 mutable struct DialectRegistry
     registry::API.MlirDialectRegistry
