900af74c75270b97de2b4d65bc20ea48e7e45ba3
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-27 15:36:52 +0100

    Refactor `Context` code

--- src/IR/Context.jl
+++ src/IR/Context.jl
@@ -32,7 +32,7 @@ Base.convert(::Core.Type{API.MlirContext}, c::Context) = c.context
 # to simplify the API, we maintain a stack of contexts in task local storage
 # and pass them implicitly to MLIR API's that require them.
 function activate!(ctx::Context)
-    stack = get!(task_local_storage(), :MLIRContext) do
+    stack = get!(task_local_storage(), :mlir_context_stack) do
         Context[]
     end
     push!(stack, ctx)
@@ -41,7 +41,7 @@ end
 
 function deactivate!(ctx::Context)
     context() == ctx || error("Deactivating wrong context")
-    pop!(task_local_storage(:MLIRContext))
+    pop!(task_local_storage(:mlir_context_stack))
 end
 
 function dispose!(ctx::Context)
@@ -49,15 +49,14 @@ function dispose!(ctx::Context)
     API.mlirContextDestroy(ctx.context)
 end
 
-_has_context() = haskey(task_local_storage(), :MLIRContext) &&
-                 !isempty(task_local_storage(:MLIRContext))
+_has_context() = haskey(task_local_storage(), :mlir_context_stack) && !isempty(task_local_storage(:mlir_context_stack))
 
 function context(; throw_error::Core.Bool=true)
     if !_has_context()
         throw_error && error("No MLIR context is active")
         return nothing
     end
-    last(task_local_storage(:MLIRContext))
+    last(task_local_storage(:mlir_context_stack))
 end
 
 function context!(f, ctx::Context)
@@ -69,9 +68,9 @@ function context!(f, ctx::Context)
     end
 end
 
-function enable_multithreading!(enable::Bool=true)
-    API.mlirContextEnableMultithreading(context(), enable)
+function enable_multithreading!(enable::Bool=true; context::Context=context())
+    API.mlirContextEnableMultithreading(context, enable)
     context()
 end
 
-Base.:(==)(a::Context, b::Context) = API.mlirContextEqual(a.context, b.context)
+Base.:(==)(a::Context, b::Context) = API.mlirContextEqual(a, b)
