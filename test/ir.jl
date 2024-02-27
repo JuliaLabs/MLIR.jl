7ddfeb4b6dbdbcc825f3b08432274673e4e56ef3
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-27 18:45:55 +0100

    Refactor `MLIRType` in tests

--- test/ir.jl
+++ test/ir.jl
@@ -4,7 +4,7 @@ using MLIR.IR, LLVM
 @testset "operation introspection" begin
     IR.context!(IR.Context()) do
         IR.get_or_load_dialect!("linalg")
-        op = arith.constant(; value=true, result=MLIRType(Bool))
+        op = arith.constant(; value=true, result=IR.Type(Bool))
 
         @test IR.name(op) == "arith.constant"
         @test IR.dialect(op) === :arith
@@ -25,6 +25,6 @@ end
         @test IR.name(op) == "builtin.module"
 
         # Only a `module` operation can be used to create a module.
-        @test_throws AssertionError IR.Module(arith.constant(; value=true, result=MLIRType(Bool)))
+        @test_throws AssertionError IR.Module(arith.constant(; value=true, result=IR.Type(Bool)))
     end
 end
