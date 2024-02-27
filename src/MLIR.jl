5bf8c28cb75fc2f907a7f39d63890ca3b0f4ad75
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-27 16:26:45 +0100

    Refactor code

--- src/MLIR.jl
+++ src/MLIR.jl
@@ -3,48 +3,24 @@ module MLIR
 import LLVM
 
 module API
-    using CEnum
+using CEnum
 
-    # MLIR C API
-    import ..LLVM
-    using MLIR_jll
-    let
-        ver = string(LLVM.version().major)
-        dir = joinpath(@__DIR__, "API", ver)
-        if !isdir(dir)
-            error("""The MLIR API bindings for v$ver do not exist.
-                    You might need a newer version of MLIR.jl for this version of Julia.""")
-        end
-
-        include(joinpath(dir, "libMLIR_h.jl"))
+# MLIR C API
+import ..LLVM
+using MLIR_jll
+let
+    ver = string(LLVM.version().major)
+    dir = joinpath(@__DIR__, "API", ver)
+    if !isdir(dir)
+        error("""The MLIR API bindings for v$ver do not exist.
+                You might need a newer version of MLIR.jl for this version of Julia.""")
     end
+
+    include(joinpath(dir, "libMLIR_h.jl"))
+end
 end # module API
 
-# MlirStringRef is a non-owning reference to a string,
-# we thus need to ensure that the Julia string remains alive
-# over the use. For that we use the cconvert/unsafe_convert mechanism
-# for foreign-calls. The returned value of the cconvert is rooted across
-# foreign-call.
-Base.cconvert(::Type{API.MlirStringRef}, s::Union{Symbol, String}) = s
-Base.cconvert(::Type{API.MlirStringRef}, s::AbstractString) =
-    Base.cconvert(API.MlirStringRef, String(s)::String)
-
-# Directly create `MlirStringRef` instead of adding an extra ccall.
-function Base.unsafe_convert(::Type{API.MlirStringRef}, s::Union{Symbol, String, AbstractVector{UInt8}})
-    p = Base.unsafe_convert(Ptr{Cchar}, s)
-    return API.MlirStringRef(p, sizeof(s))
-end
-
-function Base.String(str::API.MlirStringRef)
-    Base.unsafe_string(pointer(str.data), str.length)
-end
-
-module IR
-    import ..API: API
-
-    include("IR/IR.jl")
-    include("IR/state.jl")
-end # module IR
+include("IR/IR.jl")
 
 include("Dialects.jl")
 
