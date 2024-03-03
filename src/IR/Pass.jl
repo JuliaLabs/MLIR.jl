b01705a01cc316f1713b972558d298f9b1a6ae7d
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-03-03 15:10:28 +0100

    Refactor `PassManager`,`OpPassManager`

--- src/IR/Pass.jl
+++ src/IR/Pass.jl
@@ -1,5 +1,3 @@
-### Pass Manager
-
 abstract type AbstractPass end
 
 mutable struct ExternalPassHandle
@@ -20,28 +18,54 @@ mutable struct PassManager
     end
 end
 
+"""
+    PassManager(; context=context())
+
+Create a new top-level PassManager.
+"""
+PassManager(; context::Context=context()) = PassManager(API.mlirPassManagerCreate(context))
+
+@llvmversioned min=v"16" """
+    PassManager(anchorOp; context=context())
+
+Create a new top-level PassManager anchored on `anchorOp`.
+"""
+PassManager(anchor_op::Operation; context::Context=context()) = PassManager(API.mlirPassManagerCreateOnOperation(context, anchor_op))
+
+Base.convert(::Core.Type{API.MlirPassManager}, pass::PassManager) = pass.pass
+
+"""
+    enable_ir_printing!(passManager)
+
+Enable mlir-print-ir-after-all.
+"""
 function enable_ir_printing!(pm)
     API.mlirPassManagerEnableIRPrinting(pm)
     pm
 end
+
+"""
+    enable_verifier!(passManager, enable)
+
+Enable / disable verify-each.
+"""
 function enable_verifier!(pm, enable=true)
     API.mlirPassManagerEnableVerifier(pm, enable)
     pm
 end
 
-PassManager(; context::Context=context()) = PassManager(API.mlirPassManagerCreate(context))
+"""
+    run!(passManager, module)
 
-function run!(pm::PassManager, module_)
-    status = LogicalResult(API.mlirPassManagerRun(pm, module_))
+Run the provided `passManager` on the given `module`.
+"""
+function run!(pm::PassManager, mod::Module)
+    status = LogicalResult(API.mlirPassManagerRun(pm, mod))
     if isfailure(status)
         throw("failed to run pass manager on module")
     end
-    module_
+    mod
 end
-
-Base.convert(::Core.Type{API.MlirPassManager}, pass::PassManager) = pass.pass
-
-### Op Pass Manager
 
 struct OpPassManager
     op_pass::API.MlirOpPassManager
@@ -53,8 +77,26 @@ struct OpPassManager
     end
 end
 
+"""
+    OpPassManager(passManager)
+
+Cast a top-level `PassManager` to a generic `OpPassManager`.
+"""
 OpPassManager(pm::PassManager) = OpPassManager(API.mlirPassManagerGetAsOpPassManager(pm), pm)
+
+"""
+    OpPassManager(passManager, operationName)
+
+Nest an `OpPassManager` under the top-level PassManager, the nested passmanager will only run on operations matching the provided name.
+The returned `OpPassManager` will be destroyed when the parent is destroyed. To further nest more `OpPassManager` under the newly returned one, see `mlirOpPassManagerNest` below.
+"""
 OpPassManager(pm::PassManager, opname) = OpPassManager(API.mlirPassManagerGetNestedUnder(pm, opname), pm)
+
+"""
+    OpPassManager(opPassManager, operationName)
+
+Nest an `OpPassManager` under the provided `OpPassManager`, the nested passmanager will only run on operations matching the provided name. The returned `OpPassManager` will be destroyed when the parent is destroyed.
+"""
 OpPassManager(opm::OpPassManager, opname) = OpPassManager(API.mlirOpPassManagerGetNestedUnder(opm, opname), opm.pass)
 
 Base.convert(::Core.Type{API.MlirOpPassManager}, op_pass::OpPassManager) = op_pass.op_pass
@@ -77,6 +119,51 @@ function Base.showerror(io::IO, err::AddPipelineException)
     nothing
 end
 
+"""
+    add_owned_pass!(passManager, pass)
+
+Add a pass and transfer ownership to the provided top-level `PassManager`. If the pass is not a generic operation pass or a `ModulePass`, a new `OpPassManager` is implicitly nested under the provided PassManager.
+"""
+function add_owned_pass!(pm::PassManager, pass)
+    API.mlirPassManagerAddOwnedPass(pm, pass)
+    pm
+end
+
+"""
+    add_owned_pass!(opPassManager, pass)
+
+Add a pass and transfer ownership to the provided `OpPassManager`. If the pass is not a generic operation pass or matching the type of the provided `OpPassManager`, a new `OpPassManager` is implicitly nested under the provided `OpPassManager`.
+"""
+function add_owned_pass!(opm::OpPassManager, pass)
+    API.mlirOpPassManagerAddOwnedPass(opm, pass)
+    opm
+end
+
+"""
+    parse(passManager, pipeline)
+
+Parse a textual MLIR pass pipeline and add it to the provided `OpPassManager`.
+"""
+function Base.parse(opm::OpPassManager, pipeline::String)
+    result = if LLVM.version() >= v"16"
+        io = IOBuffer()
+        c_print_callback = @cfunction(print_callback, Cvoid, (API.MlirStringRef, Any))
+        API.mlirParsePassPipeline(opm, pipeline, c_print_callback, Ref(io))
+    else
+        API.mlirParsePassPipeline(opm, pipeline)
+    end |> LogicalResult
+    
+    if isfailure(result)
+        throw(AddPipelineException(String(take!(io))))
+    end
+    opm
+end
+
+@llvmversioned min=v"16" """
+    add_pipeline!(passManager, pipelineElements, callback, userData)
+
+Parse a sequence of textual MLIR pass pipeline elements and add them to the provided OpPassManager. If parsing fails an error message is reported using the provided callback.
+"""
 function add_pipeline!(op_pass::OpPassManager, pipeline)
     @static if isdefined(API, :mlirOpPassManagerAddPipeline)
         io = IOBuffer()
@@ -94,17 +181,6 @@ function add_pipeline!(op_pass::OpPassManager, pipeline)
     end
     op_pass
 end
-
-function add_owned_pass!(pm::PassManager, pass)
-    API.mlirPassManagerAddOwnedPass(pm, pass)
-    pm
-end
-
-function add_owned_pass!(opm::OpPassManager, pass)
-    API.mlirOpPassManagerAddOwnedPass(opm, pass)
-    opm
-end
-
 
 @static if isdefined(API, :mlirCreateExternalPass)
 
