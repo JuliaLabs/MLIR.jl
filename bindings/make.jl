f88de4bd6ecbbe1dc753393ac756294ea7065f38
Yupei Qi <qiyupei@gmail.com>
2024-04-09 09:35:09 +0900

    Apply suggested changes
    Co-authored-by: Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>

--- bindings/make.jl
+++ bindings/make.jl
@@ -168,7 +168,7 @@ for (julia_version, llvm_version) in julia_llvm
 
             libmlir_header_dir = joinpath(include_dir, "mlir-c")
             args = Generators.get_default_args(get_triple(); is_cxx=true)
-            push!(args, "-I"*include_dir)
+            push!(args, "-I$include_dir")
             push!(args, "-xc++")
 
             headers = detect_headers(libmlir_header_dir, args, Dict(), endswith("Python/Interop.h"))
