8f0a52a93b31f4be1b2472a44c17d3e713202a04
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-05-01 00:27:15 +0200

    Generate version-less dispatching module of dialects

--- src/Dialects/Dialects.jl
+++ src/Dialects/Dialects.jl
@@ -1,19 +1,8 @@
 module Dialects
 
-using ..MLIR: VersionDispatcher
-import ..IR: IR, Attribute, NamedAttribute, context
-import ..API
+include("Utils.jl")
 
-namedattribute(name, val) = namedattribute(name, Attribute(val))
-namedattribute(name, val::Attribute) = NamedAttribute(name, val)
-function namedattribute(name, val::NamedAttribute)
-    @assert true # TODO(jm): check whether name of attribute is correct, getting the name might need to be added to IR.jl?
-    return val
-end
-
-operandsegmentsizes(segments) = namedattribute("operand_segment_sizes", Attribute(Int32.(segments)))
-
-# generate versioned API modules
+# generate versioned modules
 for version in Base.Filesystem.readdir(joinpath(@__DIR__))
     isdir(joinpath(@__DIR__, version)) || continue
     includes = map(readdir(joinpath(@__DIR__, version); join=true)) do path
@@ -26,6 +15,39 @@ for version in Base.Filesystem.readdir(joinpath(@__DIR__))
     end
 end
 
-const Dispatcher = VersionDispatcher(@__MODULE__)
+begin
+    # list dialect operations
+    local dialectops = mapreduce(mergewith!(∪), [v14, v15, v16]) do mod
+        dialects = filter(names(mod; all=true)) do dialect
+            dialect ∉ [nameof(mod), :eval, :include] && !startswith(string(dialect), '#')
+        end
+
+        Dict(dialect => filter(names(getproperty(mod, dialect); all=true)) do name
+            name ∉ [dialect, :eval, :include] && !startswith(string(name), '#')
+        end for dialect in dialects)
+    end
+
+    # generate version-less dialect modules
+    for (dialect, ops) in dialectops
+        mod = @eval module $dialect
+        using ...MLIR: MLIR_VERSION
+        using ..Dialects: v14, v15, v16
+        end
+
+        for op in ops
+            @eval mod function $op(args...; kwargs...)
+                if v"14" <= MLIR_VERSION[] < v"15"
+                    v14.$dialect.$op(args...; kwargs...)
+                elseif v"15" <= MLIR_VERSION[] < v"16"
+                    v15.$dialect.$op(args...; kwargs...)
+                elseif v"16" <= MLIR_VERSION[] < v"17"
+                    v16.$dialect.$op(args...; kwargs...)
+                else
+                    error("Unsupported MLIR version $version")
+                end
+            end
+        end
+    end
+end
 
 end # module Dialects
