83596e71d19ce96fd2573d25c07f4fed5d72331b
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-05-01 00:33:18 +0200

    Generate version-less dispatching functions of API

--- src/API/API.jl
+++ src/API/API.jl
@@ -1,8 +1,5 @@
 module API
 
-using ..MLIR: VersionDispatcher
-using CEnum
-
 include("Types.jl")
 using .Types
 
@@ -12,10 +9,32 @@ for dir in Base.Filesystem.readdir(joinpath(@__DIR__))
     @eval module $(Symbol(:v, dir))
     using ...MLIR: MLIR_VERSION, MLIR_C_PATH
     using ...API.Types
+    using CEnum
     include(joinpath(@__DIR__, $dir, "libMLIR_h.jl"))
     end
 end
 
-const Dispatcher = VersionDispatcher(@__MODULE__)
+# generate version-less API functions
+begin
+    local ops = mapreduce(∪, [v14, v15, v16]) do mod
+        filter(names(mod; all=true)) do name
+            name ∉ [nameof(mod), :eval, :include] && !startswith(string(name), '#')
+        end
+    end
+
+    for op in ops
+        @eval function $op(args...; kwargs...)
+            if v"14" <= MLIR_VERSION[] < v"15"
+                v14.$op(args...; kwargs...)
+            elseif v"15" <= MLIR_VERSION[] < v"16"
+                v15.$op(args...; kwargs...)
+            elseif v"16" <= MLIR_VERSION[] < v"17"
+                v16.$op(args...; kwargs...)
+            else
+                error("Unsupported MLIR version $version")
+            end
+        end
+    end
+end
 
 end
