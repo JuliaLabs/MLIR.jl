206351d4e76d463a77c50adfd175443eb6989247
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-05-05 22:07:54 +0200

    Revert "Remove `Preferences` support"
    This reverts commit 65c10b6b4d2cf5cd5ee0e631a93cffb3407e00cf.

--- src/MLIR.jl
+++ src/MLIR.jl
@@ -1,10 +1,11 @@
 module MLIR
 
+using Preferences
 using ScopedValues
 import MLIR_jll
 
-const MLIR_VERSION = ScopedValue(Base.libllvm_version)
-const MLIR_C_PATH = ScopedValue(MLIR_jll.mlir_c)
+const MLIR_VERSION = ScopedValue(VersionNumber(@load_preference("MLIR_VERSION", Base.libllvm_version_string)))
+const MLIR_C_PATH = ScopedValue(@load_preference("MLIR_C_PATH", MLIR_jll.mlir_c))
 
 struct MLIRException <: Exception
     msg::String
